56ad20bf3748517fd4da0604f759e894d83f7a69
senjuti09 <103664577+senjuti09@users.noreply.github.com>
2024-08-21 19:31:33 +0530

    Create order_processing.py

---
+++ week_3/ex2/order_processing.py
@@ -0,0 +1,7 @@
+order_amount=150
+def apply_discount(order_amount):
+    if order_amount>100:
+        finalamount=order_amount*0.9
+    return finalamount
+print(apply_discount(order_amount))
+    
